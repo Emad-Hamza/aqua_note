0ce918bdd0599fecd4903eb32fadd9435b397977
Emad-Hamza <emad.aldin.hamza@gmail.com>
2018-12-13 20:17:17 +0200

    02-03 finished adding controls over the twig Service

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -29,8 +29,12 @@ class GenusController extends Controller
 //            'Inked!'
 //        ];
 
+        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+        $funFact = $this->get('markdown.parser')->transform($funFact);
+
         return $this->render('genus/show.html.twig', [
             'name' => $genusName,
+            'funFact' => $funFact,
 //            'notes' => $notes
 
         ]);
@@ -45,13 +49,13 @@ class GenusController extends Controller
 
         $notes = [
 
-            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' =>"Octopus aked me a riddle, outsmarted me", 'date' => 'Dec. 10, 2015' ],
-                ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' =>"I counted 8 legs... as they wrapped around me", 'date' => 'Dec. 1, 2015'],
-                    ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' =>"Inked!", 'date' => 'Aug. 20, 2015']
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => "Octopus aked me a riddle, outsmarted me", 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => "I counted 8 legs... as they wrapped around me", 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => "Inked!", 'date' => 'Aug. 20, 2015']
         ];
 
         $data = [
-            'notes' =>$notes,
+            'notes' => $notes,
         ];
 
         return new JsonResponse($data);
