9d910db10e74bc400df5188ab1e36cb57fbd9bd4
Emad-Hamza <emad.aldin.hamza@gmail.com>
2019-01-16 18:17:45 +0200

    finished 06 Forms, ended with using shared twig templates instead of duplicating code

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\Genus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -52,4 +53,34 @@ class GenusAdminController extends Controller
                'genusForm' => $form->createView()
             ]);
     }
+
+    /**
+     * @Route("/genus/{id}/edit", name="admin_genus_edit")
+     */
+    public function editAction(Request $request, Genus $genus)
+    {
+        $form = $this->createForm('AppBundle\Form\GenusFormType', $genus);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $genus = $form->getData();
+
+            $em = $this->getDoctrine();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genus);
+            $em->flush();
+
+            $this->addFlash('success', 'Genus updated!');
+
+            return $this->redirectToRoute('admin_genus_list');
+        }
+
+        return $this->render('admin/genus/edit.html.twig',
+            [
+                'genusForm' => $form->createView()
+            ]);
+    }
+
+
 }
